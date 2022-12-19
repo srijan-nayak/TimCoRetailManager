aa3b0b790c226fb15b48c244e9bfea3a367b6c76
Srijan Nayak <srijannayak0@gmail.com>
2022-12-19 09:29:30 +0530

    Create the login form with helper for password box

---
+++ TRMDesktopUI/Helpers/PasswordBoxHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TRMDesktopUI.Helpers
+{
+    public static class PasswordBoxHelper
+    {
+        public static readonly DependencyProperty BoundPasswordProperty = DependencyProperty.RegisterAttached("BoundPassword",
+            typeof(string), typeof(PasswordBoxHelper),
+            new FrameworkPropertyMetadata(string.Empty, OnBoundPasswordChanged));
+
+        public static string GetBountPassword(DependencyObject d)
+        {
+            var box = d as PasswordBox;
+            if (box != null)
+            {
+                box.PasswordChanged -= PasswordChanged;
+                box.PasswordChanged += PasswordChanged;
+            }
+
+            return d.GetValue(BoundPasswordProperty) as string;
+        }
+
+        public static void SetBoundPassword(DependencyObject d, string value)
+        {
+            if (string.Equals(value, GetBountPassword(d)))
+            {
+                return;
+            }
+
+            d.SetValue(BoundPasswordProperty, value);
+        }
+
+        private static void OnBoundPasswordChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var box = d as PasswordBox;
+
+            if (box == null)
+            {
+                return;
+            }
+
+            box.Password = GetBountPassword(d);
+        }
+
+        private static void PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            var password = sender as PasswordBox;
+
+            SetBoundPassword(password, password.Password);
+
+            password.GetType().GetMethod("Select", BindingFlags.Instance | BindingFlags.NonPublic)
+                .Invoke(password, new object[] { password.Password.Length, 0 });
+        }
+    }
+}
---
+++ TRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDesktopUI.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private string _userName;
+        private string _password;
+
+        public string UserName
+        {
+            get => _userName;
+            set
+            {
+                _userName = value;
+                NotifyOfPropertyChange(() => UserName);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public bool CanLogIn => !string.IsNullOrWhiteSpace(UserName) && !string.IsNullOrWhiteSpace(Password);
+
+        public void LogIn()
+        {
+            Console.WriteLine();
+        }
+    }
+}
